b7fa571c7c167a687dec09041e3254f9650cce2e
ulf-frostypaw <19324157@uagro.mx>
2022-10-17 13:41:31 -0500

    initial update

---
+++ src/Pages/Home/Index.jsx
@@ -0,0 +1,136 @@
+import { Link } from 'react-router-dom'
+export default function Home(){
+    return(
+        <>
+            <div id="page_wrapper">
+            <div id="header_wrapper">
+            <div id="branding_wrapper">
+            <h1><Link to="/" title="MyFursona.com">MyFursona.com</Link></h1>
+            </div>
+            <div id="navigation_wrapper">
+            <ul id="navigation">
+                <li><a href="/web/20080719163958/http://myfursona.com/" id="b_home"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/nav/home_off.gif" alt="Home"/></a></li>
+                <li><a href="/web/20080719163958/http://myfursona.com/users" id="b_users"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/nav/users_off.gif" alt="Users"/></a></li>
+                <li><a href="/web/20080719163958/http://myfursona.com/blogs" id="b_blogs"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/nav/blogs_off.gif" alt="Blogs"/></a></li>
+                <li><a href="/web/20080719163958/http://myfursona.com/images" id="b_gallery"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/nav/gallery_off.gif" alt="Gallery"/></a></li>
+                    <li><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/" id="b_forum"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/nav/forum_off.gif" alt="Forum"/></a></li>
+                </ul>
+            </div>
+            <hr/>
+            <div id="search_wrapper" class="aright">
+            <form id="searchform" name="search" method="post" action="/web/20080719163958/http://myfursona.com/search">
+                <input type="text" name="terms" id="terms" value="Search..."/>
+                <input type="submit" name="search" id="search" value="Search"/>
+            </form>
+            </div>
+                <div id="membermeta_wrapper">
+            Hello there, <span class="currentusername"><a href="/web/20080719163958/http://myfursona.com/login">~Guest!</a></span>
+            <small>(<a href="/web/20080719163958/http://myfursona.com/login">Login or Register</a>)</small>  </div>
+            </div><hr/>
+            <div id="body_wrapper">
+            <div id="body_header">
+            <div class="col_left"><h2 id="t_welcome">MyFursona is a social networking website for the furry fandom. A place to talk, meet new people and share your talent!</h2></div><hr/>
+                <div class="col_right">
+                <div id="prof">
+                <h2>~Guest</h2>
+                <div id="prof_inner">
+                <p>Want to join over <strong>3,000</strong> people in one of the most diverse furry social communities?<br/><a href="/web/20080719163958/http://myfursona.com/login"><img src="/web/20080719163958im_/http://myfursona.com/assets/images/icons/login.gif" alt="Register"/> Register now!</a></p>
+                </div>
+                </div>
+            </div>
+                
+            <br class="clr"/>
+            </div><hr/>
+            
+            <div id="body_inner">
+            <div class="col_left">
+                <div id="news">
+                <div class="boxtop"><div></div></div>
+                <div class="boxcontent">
+                <h2 class="secthead">Search enabled, all high priority bugs fixed.</h2>
+                <p>Quick update... Just as the title says, all the big bugs are fixed (as far as I can tell) and search has been re-enabled.<br/>
+            <br/>
+            Expect the PM system among other things soon.<br/>
+            <br/>
+            Go furpile!</p>
+                <div class="comments"><a href="/web/20080719163958/http://myfursona.com/update/64#comments"><strong>14</strong> comments</a></div><div class="postmeta">Posted by <a href="/web/20080719163958/http://myfursona.com/user/KovuTheHusky">@KovuTheHusky</a><br/>on Jul 7, 2008 9:35pm</div><br class="clr"/>
+                </div>
+                <div class="boxbottom"><div></div></div>
+                </div>
+                <div id="recentnews">
+                <strong><a href="/web/20080719163958/http://myfursona.com/updates">News archive &raquo;</a></strong><br/>
+                        <a href="/web/20080719163958/http://myfursona.com/update/63">Upcoming updates!</a> by <a href="user/KovuTheHusky">@KovuTheHusky</a><br/>
+                        <a href="/web/20080719163958/http://myfursona.com/update/62">MyFurmeet was fun, nom nom nom.</a> by <a href="user/KovuTheHusky">@KovuTheHusky</a><br/>
+                    </div>
+            <hr/>
+
+                <div class="information">
+                Join us on IRC now!<br/>
+                <a href="http://web.archive.org/web/20080719163958/irc://irc.furnet.org/myfursona">irc.furnet.org #myfursona</a>
+                </div>
+            <hr/>
+                
+                
+            </div>
+            
+            <div class="col_right">
+                <div id="latest" class="s3">
+                <h2 class="tim" id="t_latestblogs">Latest Blogs</h2>
+                <ul id="latestblogs">
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4183">Time to Brawl! Looking for a Challenge?</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Cecil-Sayataki">~Cecil-Sayataki</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4182">:D</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Nivek">~Nivek</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4181">FA haiku</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/IantheGecko">~IantheGecko</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4180">FA</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Jerthar">~Jerthar</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4179">YAY.</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Stahi">~Stahi</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4178">FurAffinity WAI?</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Taro">~Taro</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4177">Holy Crap Update!</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/morg">~morg</a></li>
+                        <li><strong><a href="/web/20080719163958/http://myfursona.com/blog/4176">OMG an UPDATE!?</a></strong> by <a href="/web/20080719163958/http://myfursona.com/user/Sarn+Darkholm">~Sarn Darkholm</a></li>
+                    </ul>
+                
+                <h2 class="tim" id="t_latestthreads">Latest Threads</h2>
+                <ul id="latestthreads">
+                        <li><strong><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/viewtopic.php?f=7&amp;t=535">Da-Fox's 1st fursuit head!</a></strong> by <a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/memberlist.php?mode=viewprofile&amp;u=285">da-fox</a></li>
+                        <li><strong><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/viewtopic.php?f=5&amp;t=534">Time for Some Campaignin�</a></strong> by <a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/memberlist.php?mode=viewprofile&amp;u=438">DrakkenRavenclaw</a></li>
+                        <li><strong><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/viewtopic.php?f=5&amp;t=533">Whats wrong with me?</a></strong> by <a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/memberlist.php?mode=viewprofile&amp;u=438">DrakkenRavenclaw</a></li>
+                        <li><strong><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/viewtopic.php?f=29&amp;t=532">Commissions; Full-Bodied Works Starting at $10</a></strong> by <a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/memberlist.php?mode=viewprofile&amp;u=269">Orthan</a></li>
+                        <li><strong><a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/viewtopic.php?f=23&amp;t=531">Furries With Spare Time</a></strong> by <a href="http://web.archive.org/web/20080719163958/http://www.myfursona.net/memberlist.php?mode=viewprofile&amp;u=361">Pyro</a></li>
+                    </ul>
+                
+                <h2 class="tim" id="t_newmembers">Newest Members</h2>
+                <ul id="newestmembers">
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Fox+Amoore">~Fox Amoore</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Vikt0m">~Vikt0m</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/devils">~devils</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Kano">~Kano</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/x">~x</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Midekai">~Midekai</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/iruser">~iruser</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/kingculex">~kingculex</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/suicidebycyanide">~suicidebycyanide</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/hafflepuff">~hafflepuff</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Cheetar+%27%27Spots%27%27+Cheetah">~Cheetar ''Spots'' Cheetah</a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/user/Tremaine">~Tremaine</a></li>
+                    </ul>
+                
+                <h2 class="tim" id="t_latestimages">Latest Images</h2>
+                <div class="gallery">
+                <ul>
+                        <li><a href="/web/20080719163958/http://myfursona.com/image/5096"><img src="/web/20080719163958im_/http://myfursona.com/data/thumbs/(1695078386)dog.jpg.jpg" alt="Devil Dawg"/><span>Devil Dawg</span></a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/image/5095"><img src="/web/20080719163958im_/http://myfursona.com/data/thumbs/(576830504)Guild Wars Sarn Vector.jpg.jpg" alt="Guild Wars Sarn"/><span>Guild Wars Sarn</span></a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/image/5094"><img src="/web/20080719163958im_/http://myfursona.com/data/thumbs/(1648603214)jj.jpg.jpg" alt="Who- Oh, it's Just Jake"/><span>Who- Oh, it's Just Jake</span></a></li>
+                        <li><a href="/web/20080719163958/http://myfursona.com/image/5093"><img src="/web/20080719163958im_/http://myfursona.com/data/thumbs/(1980922732)fursonas.JPG.jpg" alt="My 3 fursonas"/><span>My "3" Fursonas</span></a></li>
+                        </ul>
+                </div>
+                
+                </div>
+            </div>
+            </div>
+            </div><hr/>
+            <div id="footer_wrapper">
+            <span class="copyright">&copy;2008 MyFursona</span>
+            <span class="contact"><a href="/web/20080719163958/http://myfursona.com/about">About Us</a> - <a href="/web/20080719163958/http://myfursona.com/terms">Terms of Service</a> - <a href="http://web.archive.org/web/20080719163958/mailto:bugs@myfursona.com">Report Bug</a> - <a href="/web/20080719163958/http://myfursona.com/random/user" style="color:red;">Random!</a></span>
+            </div>
+            </div>
+        </>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+//import './index.css';
+import './global.css';
+
+import Home from './Pages/Home/Index.jsx'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} ></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
